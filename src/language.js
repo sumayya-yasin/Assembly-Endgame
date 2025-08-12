0da828fbab5fb8d22f656197e8df7ca033f06b20
sumayya-yasin <sumayyabintyasin@gmail.com>
2025-08-12 13:04:06 +0500

    Accessibility fix

--- src/language.js
+++ src/language.js
@@ -1,47 +1,47 @@
 export default [
-    {
-        name: "HTML",
-        backgroundColor: "#E2680F",
-        color: "#F9F4DA",
-    },
-    {
-        name: "CSS",
-        backgroundColor: "#328AF1",
-        color: "#F9F4DA",
-    },
-    {
-        name: "JavaScript",
-        backgroundColor: "#F4EB13",
-        color: "#1E1E1E",
-    },
-    {
-        name: "React",
-        backgroundColor: "#2ED3E9",
-        color: "#1E1E1E",
-    },
-    {
-        name: "TypeScript",
-        backgroundColor: "#298EC6",
-        color: "#F9F4DA",
-    },
-    {
-        name: "Node.js",
-        backgroundColor: "#599137",
-        color: "#F9F4DA",
-    },
-    {
-        name: "Python",
-        backgroundColor: "#FFD742",
-        color: "#1E1E1E",
-    },
-    {
-        name: "Ruby",
-        backgroundColor: "#D02B2B",
-        color: "#F9F4DA",
-    },
-    {
-        name: "Assembly",
-        backgroundColor: "#2D519F",
-        color: "#F9F4DA",
-    },
-]
+  {
+    name: "HTML",
+    backgroundColor: "#E2680F",
+    color: "#FFFFFF",
+  },
+  {
+    name: "CSS",
+    backgroundColor: "#328AF1",
+    color: "#FFFFFF",
+  },
+  {
+    name: "JavaScript",
+    backgroundColor: "#F4EB13",
+    color: "#2C2C2C",
+  },
+  {
+    name: "React",
+    backgroundColor: "#2ED3E9",
+    color: "#083344",
+  },
+  {
+    name: "TypeScript",
+    backgroundColor: "#298EC6",
+    color: "#FFFFFF",
+  },
+  {
+    name: "Node.js",
+    backgroundColor: "#599137",
+    color: "#FFFFFF",
+  },
+  {
+    name: "Python",
+    backgroundColor: "#FFD742",
+    color: "#2C2C2C",
+  },
+  {
+    name: "Ruby",
+    backgroundColor: "#D02B2B",
+    color: "#FFFFFF",
+  },
+  {
+    name: "Assembly",
+    backgroundColor: "#2D519F",
+    color: "#FFFFFF",
+  },
+];
